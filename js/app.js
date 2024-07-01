52e131ec1fe35b79398ead0d3112af9899ef08c0
Eclipse <167474735+TheEclipsefr@users.noreply.github.com>
2024-07-01 16:59:50 +0200

    made tweaks for nav bar,added comment section, smooth scroll added
    added ids for the nav bar contents while adding comment section and its nav bar button and also added smooth scrolling using js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 /** TODO: Build the navigation bar using JavaScript **/
-sect_1 = document.querySelector('#section1')
-sect_2 = document.querySelector('#section2')
-sect_3 = document.querySelector('#section3')
-sect_4 = document.querySelector('#section4')
-
-const sections_arrays = [sect_1, sect_2, sect_3, sect_4]
-console.log(sections_arrays)
 /** TODO: Add smooth scrolling **/
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Select all links with hashes
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            // Scroll to the corresponding section
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
 
 /** TODO: Add an active state **/
 
