f7a391c16d844c8a3ecc94f77e146d4d569b5b23
Eclipse <112015745+Mohamedismail231@users.noreply.github.com>
2024-07-01 13:24:23 +0200

    added cleaner code
    tweaked the HTML code and made it more readable also made a small tweak in css related to the navigation bar and added some vars and consts for js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 /** TODO: Build the navigation bar using JavaScript **/
+sect_1 = document.querySelector('#section1')
+sect_2 = document.querySelector('#section2')
+sect_3 = document.querySelector('#section3')
+sect_4 = document.querySelector('#section4')
 
+const sections_arrays = [sect_1, sect_2, sect_3, sect_4]
+console.log(sections_arrays)
 /** TODO: Add smooth scrolling **/
 
 /** TODO: Add an active state **/
